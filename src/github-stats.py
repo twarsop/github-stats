6525dd673faccedd7dbb0cd047bc9b93427dc81a
twarsop <tewarsop@gmail.com>
2024-10-27 19:34:23 +0000

    Move getting repo commit files into a function

--- src/github-stats.py
+++ src/github-stats.py
@@ -21,6 +21,20 @@ def get_user_repos(username):
     else:
         return None
 
+def get_user_repo_commit_files(username, repo):
+    commit_files = []
+    
+    url = f"{GITHUB_API_BASE_URL}/repos/{username}/{repo['name']}/commits?since=2023-10-01&until=2023-11-01"
+    commits = requests.get(url, headers=HEADERS)
+    for commit in commits.json():
+        commit_url = f"{GITHUB_API_BASE_URL}/repos/{username}/{repo['name']}/commits/{commit['sha']}"
+        commit_info = requests.get(commit_url, headers=HEADERS)
+        for file in commit_info.json()["files"]:
+            commit_files.append(CommitFile(datetime=commit["commit"]["author"]["date"], filename=file["filename"], additions=file["additions"]))
+        break
+
+    return commit_files
+
 if __name__ == "__main__":    
     username = "twarsop"
     user_repos = get_user_repos(username)
@@ -29,14 +43,7 @@ if __name__ == "__main__":
 
     if user_repos:
         for repo in user_repos:
-            url = f"{GITHUB_API_BASE_URL}/repos/{username}/{repo['name']}/commits?since=2023-10-01&until=2023-11-01"
-            commits = requests.get(url, headers=HEADERS)
-            for commit in commits.json():
-                commit_url = f"{GITHUB_API_BASE_URL}/repos/{username}/{repo['name']}/commits/{commit['sha']}"
-                commit_info = requests.get(commit_url, headers=HEADERS)
-                for file in commit_info.json()["files"]:
-                    commit_files.append(CommitFile(datetime=commit["commit"]["author"]["date"], filename=file["filename"], additions=file["additions"]))
-                break
+            commit_files += get_user_repo_commit_files(username, repo)
             break
     else:
         print(f"Failed to retrieve repositories.")
