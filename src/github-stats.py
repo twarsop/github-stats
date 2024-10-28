30b72634af5e23ab089d63999ebb43f5b0abced5
twarsop <tewarsop@gmail.com>
2024-10-28 20:00:22 +0000

    Move everything into one function

--- src/github-stats.py
+++ src/github-stats.py
@@ -12,40 +12,37 @@ class CommitFile:
     filename: str
     additions: int
 
-def get_user_repos(username):
-    url = f"{GITHUB_API_BASE_URL}/users/{username}/repos"
+def get_commit_files(github_username, commits_since_date, commits_until_date):
+    commit_files = []
+    
+    repositories_data = None
+    
+    url = f"{GITHUB_API_BASE_URL}/users/{github_username}/repos"
     response = requests.get(url, headers=HEADERS)
     if response.status_code == 200:
         repositories_data = response.json()
-        return repositories_data
     else:
         return None
-
-def get_user_repo_commit_files(username, repo):
-    commit_files = []
     
-    url = f"{GITHUB_API_BASE_URL}/repos/{username}/{repo['name']}/commits?since=2023-10-01&until=2023-11-01"
-    commits = requests.get(url, headers=HEADERS)
-    for commit in commits.json():
-        commit_url = f"{GITHUB_API_BASE_URL}/repos/{username}/{repo['name']}/commits/{commit['sha']}"
-        commit_info = requests.get(commit_url, headers=HEADERS)
-        for file in commit_info.json()["files"]:
-            commit_files.append(CommitFile(datetime=commit["commit"]["author"]["date"], filename=file["filename"], additions=file["additions"]))
-        break
+    if repositories_data:
+        for repo in repositories_data:
+            url = f"{GITHUB_API_BASE_URL}/repos/{github_username}/{repo['name']}/commits?since={commits_since_date}&until={commits_until_date}"
+            commits = requests.get(url, headers=HEADERS)
+            for commit in commits.json():
+                commit_url = f"{GITHUB_API_BASE_URL}/repos/{github_username}/{repo['name']}/commits/{commit['sha']}"
+                commit_info = requests.get(commit_url, headers=HEADERS)
+                for file in commit_info.json()["files"]:
+                    commit_files.append(CommitFile(datetime=commit["commit"]["author"]["date"], filename=file["filename"], additions=file["additions"]))
+                break
 
-    return commit_files
-
-if __name__ == "__main__":    
-    username = "twarsop"
-    user_repos = get_user_repos(username)
-
-    commit_files = []
-
-    if user_repos:
-        for repo in user_repos:
-            commit_files += get_user_repo_commit_files(username, repo)
             break
     else:
         print("Failed to retrieve repositories.")
+
+    return commit_files
+    
+
+if __name__ == "__main__":    
+    commit_files = get_commit_files("twarsop", "2023-10-01", "2023-11-01")
 
     print(commit_files)
