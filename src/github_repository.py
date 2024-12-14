2d4f2a05242ea9ffdfdf2e7d0eb37ad0bcaaa7a0
twarsop <tewarsop@gmail.com>
2024-12-14 20:49:30 +0000

    Use the actual username that's passed in

--- src/github_repository.py
+++ src/github_repository.py
@@ -15,23 +15,23 @@ class CommitFile:
 def get_commit_files(github_username, commits_since_date, commits_until_date):
     commit_files = []
     
-    repositories_json = get_github_json(f"search/repositories?q=user:twarsop")
+    repositories_json = get_github_json(f"search/repositories?q=user:{github_username}", github_username)
 
     if repositories_json:
         for repo in repositories_json["items"]:
-            commits_json = get_github_json(f"repos/{github_username}/{repo['name']}/commits?since={commits_since_date}&until={commits_until_date}")
+            commits_json = get_github_json(f"repos/{github_username}/{repo['name']}/commits?since={commits_since_date}&until={commits_until_date}", github_username)
             if commits_json:
                 for commit in commits_json:
-                    commit_json = get_github_json(f"repos/{github_username}/{repo['name']}/commits/{commit['sha']}")
+                    commit_json = get_github_json(f"repos/{github_username}/{repo['name']}/commits/{commit['sha']}", github_username)
                     if commit_json:
                         for file in commit_json["files"]:
                             commit_files.append(CommitFile(datetime=datetime.strptime(commit["commit"]["author"]["date"], '%Y-%m-%dT%H:%M:%SZ'), filename=file["filename"], additions=file["additions"]))
 
     return commit_files
     
-def get_github_json(endpoint):
+def get_github_json(endpoint, github_username):
     url = f"{GITHUB_API_BASE_URL}/{endpoint}"
-    response = requests.get(url, headers=HEADERS, params={"username": "twarsop"})
+    response = requests.get(url, headers=HEADERS, params={"username": github_username})
     if response.status_code == 200:
         return response.json()
     else:
